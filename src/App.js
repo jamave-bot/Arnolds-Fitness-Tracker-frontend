ed35ff9c3d5aa733cccc075cb2036d8a783b861e
Muhammad Zahid <muhammadali775@gmail.com>
2021-06-23 14:07:07 -0400

    Completed Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './Components/Header';
+import SearchBar from './Components/SearchBar.jsx';
+import LogWokout from './Components/LogWorkout'
+import WorkoutContainer from './Components/WorkOutContainer'
+
+
+
+export default class App extends Component {
+
+
+  state= {
+
+
+
+  }
+
+
+
+
+
+
+  render() {
+    return (
+      <div>
+         <Header />
+         <LogWokout />
+         <SearchBar />
+         <WorkoutContainer />
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+
+
+
+
+export default class Header extends Component {
+
+
+
+
+
+
+
+
+
+    render() {
+        return (
+            <div>
+                <h1 id="main-header">Arnold's Fitness Tracker</h1>
+            </div>
+        )
+    }
+}
