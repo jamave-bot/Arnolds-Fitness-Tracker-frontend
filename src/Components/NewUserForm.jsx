e0c70e13d9bd4e3495418f9427743b66ce35e4a4
Muhammad Zahid <muhammadali775@gmail.com>
2021-06-24 16:15:20 -0400

    user form progress 2

--- src/Components/NewUserForm.jsx
+++ src/Components/NewUserForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button, Form } from 'semantic-ui-react'
 
 export default class NewUserForm extends Component {
 
@@ -13,49 +14,77 @@ export default class NewUserForm extends Component {
         professorName:""
     }
 
-    // handleChange = (evt)=>{
-    //     this.setState({
-    //         [evt.target.name]: evt.target.value
-    //     })
-    // }
+    handleChange = (evt)=>{
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
 
-    // handleSubmit = (e)=>{
-    //     e.preventDefault()
+    handleSubmit = (e)=>{
+        e.preventDefault()
+        
+        fetch("http://localhost:9393/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(this.state),
+        })
+        .then((r) => r.json())
+        .then((userObj) =>{
+            console.log(userObj)
+            // this.props.getWorkOutObj(workoutObj)
+            this.setState({               
+                name: "",
+                age: "",
+                height:"",
+                weight:"",
+                bodyfat:"",
+                password:"",
+            })
+            // this.props.handleClick()
+            // this.props.toggleExerciseForm()
+        });
 
-    //     fetch("http://localhost:9393/workouts", {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //         },
-    //         body: JSON.stringify(this.state),
-    //     })
-    //     .then((r) => r.json())
-    //     .then((workoutObj) =>{
-    //         console.log(workoutObj)
-    //         this.props.getWorkOutObj(workoutObj)
-    //         this.setState({
-    //             name: '',
-    //             body_part:''
-    //         })
-    //         this.props.handleClick()
-    //         this.props.toggleExerciseForm()
-    //     });
-
-    // }
+    }
 
     render() {
+        console.log(this.state)
         return (
             <>
                 <Form onSubmit={this.handleSubmit}>
                     <Form.Field>
-                    <label>New Workout?</label> <br></br>
-                    <input placeholder='Name of Workout' onChange={this.handleChange} name="name" value={this.state.name}/>
+                    <h2></h2>    
+                    <label>Not a member? Sign up!</label> <br></br>
+                    <input placeholder='Name' onChange={this.handleChange} name="name" value={this.state.name} />
                     </Form.Field>
+
                     <Form.Field>
-                    <label>Type</label> <br></br>
-                    <input placeholder="What're you working out?" onChange={this.handleChange} name="body_part" value={this.state.body_part}/>
+                    <input placeholder='Age' onChange={this.handleChange} name="age" value={this.state.age} />
                     </Form.Field>
-                    <Button type='submit'>Create Workout!</Button>
+
+                    <Form.Field>
+                    <input placeholder='Height' onChange={this.handleChange} name="height" value={this.state.height} />
+                    </Form.Field>
+
+                    <Form.Field>
+                    <input placeholder='Weight' onChange={this.handleChange} name="weight" value={this.state.weight} />
+                    </Form.Field>
+
+                   
+                    <Form.Field>
+                    <input placeholder="Body Fat Percentage" onChange={this.handleChange} name="bodyfat" value={this.state.bodyfat}/>
+                    </Form.Field>
+
+                    <Form.Field>
+                    <input placeholder="Choose a Password" onChange={this.handleChange} name="password" value={this.state.password}/>
+                    </Form.Field>
+
+                    <Form.Field>
+                    <input placeholder="Professor Name" onChange={this.handleChange} name="professorName" value={this.state.professorName}/>
+                    </Form.Field>
+
+                    <Button type='submit' onClick={this.handleSubmit}>Create User!</Button>
                 </Form>
             </>
         )
