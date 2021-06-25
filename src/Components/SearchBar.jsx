b74a8fe10dd7227ea9ef5c87eb4db3334e8a956e
Muhammad Zahid <muhammadali775@gmail.com>
2021-06-25 00:13:41 -0400

    search bar styling

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -14,10 +14,15 @@ export default class SearchBar extends Component {
       
       
         return (
-            <div>
-              <h4>Search by Workout Name</h4>
-              <input type="text" className="searchTerm" onChange={this.filterHandler} value={this.props.filter} />
+                      
+           <div class="ui fluid category search">
+             <div class="ui icon input">
+               <input class="prompt" type="text" placeholder="Workouts by Name..." type="text" className="searchTerm" onChange={this.filterHandler} value={this.props.filter} />
+               <i class="search icon"></i>
+             </div>
+             <div class="results"></div>
             </div>
-        )
+
+       )
     }
 }
